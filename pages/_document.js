7466f13c0c8beaed54eecdb088207c2600c5b7e6
txdgidraph <gmwangi103@gmail.com>
2023-04-18 02:28:55 +0300

    Added support for Adsense

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function MyDocument() {
+  useEffect(() => {
+    var ads = document.getElementsByClassName("adsbygoogle").length;
+    for (var i = 0; i < ads; i++) {
+      try {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      } catch (e) {}
+    }
+  }, []);
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7848701713514646"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
 
 MyDocument.getInitialProps = async (ctx) => {
--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,16 @@ export default function Home({
       </Head>
 
       {/* <Header /> */}
-       <JustInSection data={JUST_IN_posts}/>
-       <ExploreMoreOptions/>
+      <JustInSection data={JUST_IN_posts} />
+      <ExploreMoreOptions />
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-7848701713514646"
+        data-ad-slot="9408999505"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
       {/* <TrendingNews
         Trend_News_Data={TREND_NEWS_posts}
         Just_In_Data={JUST_IN_posts}
